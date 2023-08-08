4324c4eb7e83ae6d315758692554c97bb2497aaf
김진광 <113023365+kjink98@users.noreply.github.com>
2023-08-08 18:14:27 +0900

    15
    header, visual

--- jingwang/FrontEnd/starbucks/js/main.js
+++ jingwang/FrontEnd/starbucks/js/main.js
@@ -20,3 +20,26 @@ searchInputEl.addEventListener("blur",()=>{
     // 통합검색 placeholder 속성 제거
     searchInputEl.setAttribute("placeholder", "");
 });
+
+const badgeE1 = document.querySelector('header .badges')
+
+// lodash cdn 검색
+// 열, 객체 및 문자열 반복 / 복합적인 함수 생성
+// throttle : 속도 조절
+// 0.3초 간격으로 함수를 호출하겠다.
+// window.addEventListener('scroll', _.throttle(()=>{
+//     // console.log(window.scrollY);
+//     if(window.scrollY > 500)
+//         badgeE1.style.display = 'none';
+//     else badgeE1.style.display = 'block';
+// }, 300))
+
+// gsap cdn 검색
+// 애니메이션 자연스럽게
+window.addEventListener('scroll', _.throttle(()=>{
+    // console.log(window.scrollY);
+    if(window.scrollY > 500 )
+        // gsap.to(요소, 지속시간, 옵션)
+        gsap.to(badgeE1, .6, {opacity:0, display:'none'})
+    else gsap.to(badgeE1, .6, {opacity:1, display:'block'})
+}, 300))
