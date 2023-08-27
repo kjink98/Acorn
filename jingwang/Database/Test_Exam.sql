f369e7b2cef39a5cc46be77c90aa76cda324dd47
김진광 <113023365+kjink98@users.noreply.github.com>
2023-08-27 23:28:23 +0900

    26-1
    db exam

--- jingwang/Database/Test_Exam.sql
+++ jingwang/Database/Test_Exam.sql
@@ -227,7 +227,134 @@ ALTER TABLE textbook_order
 
 
 
+-- 학생, 강사, 직원 테이블 먼저 작성 사람 먼저 집어넣고 나머지
 
+-- employee 테이블에 데이터 삽입
+INSERT INTO employee (employee_jomin, name, residence)
+VALUES ('960405-2205454', '김지영', '서울');
+INSERT INTO employee (employee_jomin, name, residence)
+VALUES ('801212-1234567', '이민준', '서울');
+INSERT INTO employee (employee_jomin, name, residence)
+VALUES ('920301-2345677', '박서연', '서울');
+INSERT INTO employee (employee_jomin, name, residence)
+VALUES ('751101-1345597', '최현우', '서울');
+INSERT INTO employee (employee_jomin, name, residence)
+VALUES ('880123-2314545', '정서연', '서울');
+INSERT INTO employee (employee_jomin, name, residence)
+VALUES ('970528-2312123', '강민서', '서울');
+INSERT INTO employee (employee_jomin, name, residence)
+VALUES ('980421-2310415', '윤하은', '서울');
+INSERT INTO employee (employee_jomin, name, residence)
+VALUES ('710103-2231202', '임지원', '서울');
+INSERT INTO employee (employee_jomin, name, residence)
+VALUES ('660301-1313545', '한준우', '서울');
+INSERT INTO employee (employee_jomin, name, residence)
+VALUES ('600101-2312151', '송서윤', '서울');
+INSERT INTO employee (employee_jomin, name, residence)
+VALUES ('920404-2331511', '황은서', '서울');
+INSERT INTO employee (employee_jomin, name, residence)
+VALUES ('990413-1325196', '김민준', '서울');
+INSERT INTO employee (employee_jomin, name, residence)
+VALUES ('930913-2231564', '이서연', '서울');
+INSERT INTO employee (employee_jomin, name, residence)
+VALUES ('001231-2315683', '박민서', '서울');
+INSERT INTO employee (employee_jomin, name, residence)
+VALUES ('010628-2165488', '최민서', '서울');
+INSERT INTO employee (employee_jomin, name, residence)
+VALUES ('880510-1351585', '정현우', '인천');
+INSERT INTO employee (employee_jomin, name, residence)
+VALUES ('940817-1359848', '강서준', '인천');
+INSERT INTO employee (employee_jomin, name, residence)
+VALUES ('930303-2015894', '윤지원', '서울');
+INSERT INTO employee (employee_jomin, name, residence)
+VALUES ('860218-2754321', '임지윤', '서울');
+INSERT INTO employee (employee_jomin, name, residence)
+VALUES ('890920-2851848', '한지윤', '서울');
+
+-- instructor 테이블에 데이터 삽입
+INSERT INTO instructor (instructor_jumin, name, residence, license_number, license_name)
+VALUES ('900117-2151850', '송민서', '하남', 001, '교육자격증');
+INSERT INTO instructor (instructor_jumin, name, residence, license_number, license_name)
+VALUES ('890208-2118515', '황지윤', '서울', 002, '교사자격증');
+INSERT INTO instructor (instructor_jumin, name, residence, license_number, license_name)
+VALUES ('580606-1235458', '김예준', '울진', 001, '교육자격증');
+INSERT INTO instructor (instructor_jumin, name, residence, license_number, license_name)
+VALUES ('920713-1357004', '이서준', '경기도 광주', 001, '교육자격증');
+INSERT INTO instructor (instructor_jumin, name, residence, license_number, license_name)
+VALUES ('910920-1507404', '박민준', '서울', 123456, '언어자격증');
+INSERT INTO instructor (instructor_jumin, name, residence, license_number, license_name)
+VALUES ('901101-2851848', '최서준', '시흥', 001, '교육자격증');
+INSERT INTO instructor (instructor_jumin, name, residence, license_number, license_name)
+VALUES ('700823-2154034', '정주아', '서울', 002, '교사자격증');
+INSERT INTO instructor (instructor_jumin, name, residence, license_number, license_name)
+VALUES ('890421-2351401', '강하윤', '인천', 001, '교육자격증');
+INSERT INTO instructor (instructor_jumin, name, residence, license_number, license_name)
+VALUES ('940220-2240804', '윤민서', '부천', 123456, '언어자격증');
+INSERT INTO instructor (instructor_jumin, name, residence, license_number, license_name)
+VALUES ('951030-2158100', '임하윤', '제주', 001, '교육자격증');
+INSERT INTO instructor (instructor_jumin, name, residence, license_number, license_name)
+VALUES ('770620-2480748', '한주아', '인천', 002, '교사자격증');
+INSERT INTO instructor (instructor_jumin, name, residence, license_number, license_name)
+VALUES ('820530-2013220', '송주아', '속초', 002, '교사자격증');
+INSERT INTO instructor (instructor_jumin, name, residence, license_number, license_name)
+VALUES ('870130-1201303', '황현우', '서울', 002, '교사자격증');
+INSERT INTO instructor (instructor_jumin, name, residence, license_number, license_name)
+VALUES ('910321-2113048', '김하윤', '서울', 001, '교육자격증');
+INSERT INTO instructor (instructor_jumin, name, residence, license_number, license_name)
+VALUES ('900703-1324854', '이주원', '서울', 002, '교사자격증');
+INSERT INTO instructor (instructor_jumin, name, residence, license_number, license_name)
+VALUES ('800430-1310502', '박현우', '인천', 002, '교사자격증');
+INSERT INTO instructor (instructor_jumin, name, residence, license_number, license_name)
+VALUES ('890201-2385401', '최지윤', '인천', 100100, '상담자격증');
+INSERT INTO instructor (instructor_jumin, name, residence, license_number, license_name)
+VALUES ('810303-2101854', '정지원', '서울', 002, '교사자격증');
+INSERT INTO instructor (instructor_jumin, name, residence, license_number, license_name)
+VALUES ('880130-1301054', '강민준', '서울', 100100, '상담자격증');
+INSERT INTO instructor (instructor_jumin, name, residence, license_number, license_name)
+VALUES ('870212-1232121', '윤현우', '서울', 002, '교사자격증');
+
+
+-- student 테이블에 데이터 삽입
+INSERT INTO student (student_jumin, register_date, name, residence, tuition_date)
+VALUES ('050607-2218574', TO_DATE('2023-07-29', 'YYYY-MM-DD'), '임지유', '서울', TO_DATE('2023-07-29', 'YYYY-MM-DD'));
+INSERT INTO student (student_jumin, register_date, name, residence, tuition_date)
+VALUES ('061224-2012122', TO_DATE('2022-07-15', 'YYYY-MM-DD'), '한지유', '서울', TO_DATE('2023-07-29', 'YYYY-MM-DD'));
+INSERT INTO student (student_jumin, register_date, name, residence, tuition_date)
+VALUES ('050310-2045748', TO_DATE('2021-02-22', 'YYYY-MM-DD'), '이지은', '서울', TO_DATE('2023-07-28', 'YYYY-MM-DD'));
+INSERT INTO student (student_jumin, register_date, name, residence, tuition_date)
+VALUES ('070122-1385788', TO_DATE('2023-03-03', 'YYYY-MM-DD'), '황민준', '서울', TO_DATE('2023-07-20', 'YYYY-MM-DD'));
+INSERT INTO student (student_jumin, register_date, name, residence, tuition_date)
+VALUES ('071001-1238785', TO_DATE('2023-03-03', 'YYYY-MM-DD'), '김민준', '서울', TO_DATE('2023-07-29', 'YYYY-MM-DD'));
+INSERT INTO student (student_jumin, register_date, name, residence, tuition_date)
+VALUES ('050401-1385848', TO_DATE('2021-01-29', 'YYYY-MM-DD'), '이현우', '서울', TO_DATE('2023-07-27', 'YYYY-MM-DD'));
+INSERT INTO student (student_jumin, register_date, name, residence, tuition_date)
+VALUES ('050812-1385484', TO_DATE('2022-12-13', 'YYYY-MM-DD'), '박주원', '서울', TO_DATE('2023-07-28', 'YYYY-MM-DD'));
+INSERT INTO student (student_jumin, register_date, name, residence, tuition_date)
+VALUES ('060424-1248534', TO_DATE('2022-01-10', 'YYYY-MM-DD'), '박주원', '서울', TO_DATE('2023-08-03', 'YYYY-MM-DD'));
+INSERT INTO student (student_jumin, register_date, name, residence, tuition_date)
+VALUES ('070203-1011302', TO_DATE('2023-02-24', 'YYYY-MM-DD'), '정민준', '서울', TO_DATE('2023-07-25', 'YYYY-MM-DD'));
+INSERT INTO student (student_jumin, register_date, name, residence, tuition_date)
+VALUES ('070223-1385488', TO_DATE('2023-04-23', 'YYYY-MM-DD'), '강민재', '서울', TO_DATE('2023-07-23', 'YYYY-MM-DD'));
+INSERT INTO student (student_jumin, register_date, name, residence, tuition_date)
+VALUES ('051203-1018772', TO_DATE('2021-09-19', 'YYYY-MM-DD'), '윤현준', '서울', TO_DATE('2023-07-29', 'YYYY-MM-DD'));
+INSERT INTO student (student_jumin, register_date, name, residence, tuition_date)
+VALUES ('060607-1354874', TO_DATE('2022-03-20', 'YYYY-MM-DD'), '임주영', '인천', TO_DATE('2023-07-29', 'YYYY-MM-DD'));
+INSERT INTO student (student_jumin, register_date, name, residence, tuition_date)
+VALUES ('051102-2321857', TO_DATE('2023-06-03', 'YYYY-MM-DD'), '한주영', '서울', TO_DATE('2023-07-28', 'YYYY-MM-DD'));
+INSERT INTO student (student_jumin, register_date, name, residence, tuition_date)
+VALUES ('050401-1010205', TO_DATE('2021-01-05', 'YYYY-MM-DD'), '송민준', '가평', TO_DATE('2023-08-05', 'YYYY-MM-DD'));
+INSERT INTO student (student_jumin, register_date, name, residence, tuition_date)
+VALUES ('050505-2202055', TO_DATE('2022-08-02', 'YYYY-MM-DD'), '황지영', '서울', TO_DATE('2023-07-29', 'YYYY-MM-DD'));
+INSERT INTO student (student_jumin, register_date, name, residence, tuition_date)
+VALUES ('060606-2121213', TO_DATE('2023-06-01', 'YYYY-MM-DD'), '이하이', '서울', TO_DATE('2023-07-29', 'YYYY-MM-DD'));
+INSERT INTO student (student_jumin, register_date, name, residence, tuition_date)
+VALUES ('070707-2315487', TO_DATE('2023-04-10', 'YYYY-MM-DD'), '이다영', '서울', TO_DATE('2023-08-10', 'YYYY-MM-DD'));
+INSERT INTO student (student_jumin, register_date, name, residence, tuition_date)
+VALUES ('050524-2321587', TO_DATE('2023-07-01', 'YYYY-MM-DD'), '박민주', '서울', TO_DATE('2023-07-29', 'YYYY-MM-DD'));
+INSERT INTO student (student_jumin, register_date, name, residence, tuition_date)
+VALUES ('050430-2158873', TO_DATE('2021-10-30', 'YYYY-MM-DD'), '최민주', '서울', TO_DATE('2023-07-29', 'YYYY-MM-DD'));
+INSERT INTO student (student_jumin, register_date, name, residence, tuition_date)
+VALUES ('050202-1011012', TO_DATE('2023-07-29', 'YYYY-MM-DD'), '정준우', '서울', TO_DATE('2023-07-29', 'YYYY-MM-DD'));
 
 -- classroom 테이블에 데이터 삽입
 INSERT INTO classroom (classroom_id, computer_amount, project_device, classroom_name)
@@ -237,33 +364,25 @@ VALUES (1, 20, 'Projector', 'A101');
 INSERT INTO course (course_code, course_instructor, assignment_classroom, tuition, classroom_id, textbook_id)
 VALUES (1, '강사A', 'A101', 100000, 1, 1);
 
--- employee 테이블에 데이터 삽입
-INSERT INTO employee (employee_jomin, name, residence)
-VALUES ('12345678901234567890', '직원A', '서울시');
-
 -- employee_wage 테이블에 데이터 삽입
 INSERT INTO employee_wage (employee_jomin, salary)
-VALUES ('12345678901234567890', 3000000);
+VALUES ('050607-2218574', 3000000);
 
--- instructor 테이블에 데이터 삽입
-INSERT INTO instructor (instructor_jumin, name, residence, license_number, license_name)
-VALUES ('09876543210987654321', '강사A', '경기도', 123456, '교육자격증');
+
 
 -- instructor_wage 테이블에 데이터 삽입
 INSERT INTO instructor_wage (instructor_jumin, academy_exists, hourly_wage)
-VALUES ('09876543210987654321', 'Y', 25000);
+VALUES ('890920-2851848', 'Y', 25000);
 
 -- lecture 테이블에 데이터 삽입
 INSERT INTO lecture (course_code, instructor_jumin, lecture_code)
-VALUES (1, '09876543210987654321', 1);
+VALUES (1, '890920-2851848', 1);
 
 -- registers 테이블에 데이터 삽입
 INSERT INTO registers (student_jumin, course_code, class_code, class_date)
-VALUES ('12345678901234567890', 1, 1, TO_DATE('2023-08-20', 'YYYY-MM-DD'));
+VALUES ('050607-2218574', 1, 1, TO_DATE('2023-08-20', 'YYYY-MM-DD'));
 
--- student 테이블에 데이터 삽입
-INSERT INTO student (student_jumin, register_date, name, residence, tuition_date)
-VALUES ('12345678901234567890', TO_DATE('2023-08-01', 'YYYY-MM-DD'), '학생A', '서울시', TO_DATE('2023-08-01', 'YYYY-MM-DD'));
+
 
 -- supplier 테이블에 데이터 삽입
 INSERT INTO supplier (supplier_id, name, pament_date)
