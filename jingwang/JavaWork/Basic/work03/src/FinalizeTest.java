eb0b1d1c2377937c9aff3060d3f940584871d369
김진광 <113023365+kjink98@users.noreply.github.com>
2023-08-29 16:40:16 +0900

    28
    java

---
+++ jingwang/JavaWork/Basic/work03/src/FinalizeTest.java
@@ -0,0 +1,18 @@
+class Fdemo {
+	int count;
+	
+	Fdemo(int cnt){
+		count = cnt;
+	}
+	
+	protected void finalize(){
+		System.out.println("GC °¡µ¿Áß : " + count);
+	}
+}
+public class FinalizeTest{
+	public static void main(String[] args){
+		for(int i = 0; i <500000; i++){
+			new Fdemo(i);
+		}
+	}
+}
---
+++ jingwang/JavaWork/Basic/work04/src/ArrayTest1.java
@@ -0,0 +1,5 @@
+public class ArrayTest1 {
+	public static void main(String[] args) {
+		System.out.println("test...");
+	}
+}
