326eeea465a0598c470983339e86cbdc86469a00
김진광 <113023365+kjink98@users.noreply.github.com>
2023-09-14 11:39:56 +0900

    40. 이클립스 시작 전 중간 커밋.

--- JspWork/Manual/ServletProject2/WEB-INF/src/IndexServlet.java
+++ JspWork/Manual/ServletProject2/WEB-INF/src/IndexServlet.java
@@ -1,8 +1,16 @@
 import javax.servlet.http.*;
+import java.io.*;
 
+// javac -d ../classes .\IndexServlet.java
 public class IndexServlet extends HttpServlet {
-		protected void	doGet(HttpServletRequest req, HttpServletResponse resp){
-			System.out.println("Get Test ...");
+		protected void	doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException{
+			//System.out.println("Get Test ...");
+			
+			// Returns a PrintWriter object that can send character text to the client
+			PrintWriter out = resp.getWriter();
+			// need to decorate html inside Java
+			out.println("<html><body><h1>Good Request~<h1></body></html>");
+			out.close();
 	}
 
 		protected void	doPost(HttpServletRequest req, HttpServletResponse resp){
