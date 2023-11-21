f343df61395b8060f9efe62c83af3507b5d9e264
김진광 <113023365+kjink98@users.noreply.github.com>
2023-11-21 16:37:49 +0900

    82. React_SampleTest

---
+++ FrontEnd/react_exam/4_Examples/simple-exam/src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [regions, setRegions] = useState([{ kor: "서울", eng: 'seoul' },
+  { kor: "부산", eng: 'busan' },
+  { kor: "제주", eng: 'jeju' }
+  ]);
+  const changeHandler = (e) => {
+    console.log(e.target.value);
+  }
+
+  return (
+    <div>
+      <h1>라디오 버튼의 value를 출력</h1>
+      <select onChange={changeHandler}>
+        {
+          regions.map(region => (
+            <option key={region.eng} value={region.eng}>{region.kor}</option>
+          ))
+        }
+        {/* <option key="seoul" value="seoul">서울</option>
+        <option key="busan" value="busan">부산</option>
+        <option key="jeju" value="jeju">제주</option> */}
+      </select>
+      <button>확인</button>
+    </div>
+  );
+}
+
+export default App;
